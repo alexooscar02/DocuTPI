1ea51d9f6b94c9598ee2e15e1f1599a342096a86
home <home@DESKTOP-NOCTF6P>
2024-04-02 17:55:14 -0600

    saludo bean

---
+++ src/test/java/sv/edu/ues/occ/ingenieria/tpi135/documientos/boundary/rest/TipoAtributoResourceTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sv.edu.ues.occ.ingenieria.tpi135.documientos.boundary.rest;
+
+/**
+ *
+ * @author home
+ */
+public class TipoAtributoResourceTest {
+ 
+    
+    
+    
+}
