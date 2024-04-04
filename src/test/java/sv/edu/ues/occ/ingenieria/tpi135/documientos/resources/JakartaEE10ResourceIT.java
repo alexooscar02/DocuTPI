114d7d297d9d933facc9ec46bd53b0bfbe01adc4
home <home@DESKTOP-NOCTF6P>
2024-04-03 23:11:27 -0600

    antes de la imagen de payara

--- src/test/java/sv/edu/ues/occ/ingenieria/tpi135/documientos/resources/JakartaEE10ResourceIT.java
+++ src/test/java/sv/edu/ues/occ/ingenieria/tpi135/documientos/resources/JakartaEE10ResourceIT.java
@@ -1,15 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package sv.edu.ues.occ.ingenieria.tpi135.documientos.resources;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.Assertions;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Order;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.MountableFile;
 
 /**
  *
@@ -18,16 +23,34 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public class JakartaEE10ResourceIT {
 
+    MountableFile war = MountableFile.forHostPath(Paths.get("target/Documientos-1.0-SNAPSHOT.war").toAbsolutePath(), 0777);
+
     @Container
     GenericContainer payara = new GenericContainer("payara/server-full:6.2024.1-jdk17")
+            .withCopyFileToContainer(war, "/opt/payara/deployments/aplicacion.war")
             .withExposedPorts(8080);
 
-    @Test
-    public void testPing() {
-
+  @Test
+    @Order(1)
+    public void testPing() throws Exception {
         System.out.println("pingIntegracion");
-//        fail("the test case  is a prototype");
+        //http://localhost:8080/aplicacion/resources/jakartaee10
+
+        //        fail("The test case is a prototype.");
         Assertions.assertTrue(payara.isRunning());
+        int esperado = 200;
+
+        HttpRequest req = HttpRequest.newBuilder()
+                .uri(new URI(String.format("http://localhost:%d/aplicacion/resources/jakartaee10", payara.getMappedPort(8080))))
+                .GET()
+                .build();
+
+        HttpClient cliente = HttpClient.newBuilder().build();
+
+        HttpResponse<String> resp = cliente.send(req, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertEquals(esperado, resp.statusCode());
+        System.out.println(resp.body());
     }
 
 }
