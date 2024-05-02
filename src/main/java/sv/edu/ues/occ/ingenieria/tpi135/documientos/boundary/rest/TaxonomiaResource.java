6a185ab3ee122ef452d9489d577852ed0e778bc4
alexooscar02 <gs21009@ues.edu.sv>
2024-05-01 19:58:53 -0600

    Creacion de GETs de TaxonomiaResource

--- src/main/java/sv/edu/ues/occ/ingenieria/tpi135/documientos/boundary/rest/TaxonomiaResource.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/tpi135/documientos/boundary/rest/TaxonomiaResource.java
@@ -6,24 +6,26 @@ package sv.edu.ues.occ.ingenieria.tpi135.documientos.boundary.rest;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 import java.io.Serializable;
 import java.net.URI;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import sv.edu.ues.occ.ingenieria.tpi135.documientos.Control.TaxonomiaBean;
 import sv.edu.ues.occ.ingenieria.tpi135.documientos.entity.Documento;
 import sv.edu.ues.occ.ingenieria.tpi135.documientos.entity.Taxonomia;
-import sv.edu.ues.occ.ingenieria.tpi135.documientos.entity.TipoDocumento;
-import sv.edu.ues.occ.ingenieria.tpi135.documientos.Control.DocumentoBean;
-import sv.edu.ues.occ.ingenieria.tpi135.documientos.Control.TipoDocumentoBean;
 
 /**
  *
@@ -34,6 +36,40 @@ public class TaxonomiaResource implements Serializable {
 
     @Inject
     TaxonomiaBean tBean;
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Taxonomia> findRange(
+            @QueryParam(value = "first")
+            @DefaultValue(value = "0") int first,
+            @QueryParam(value = "pagesize")
+            @DefaultValue(value = "50") int pageSize
+    ) {
+        if (first >= 0 && pageSize > 0) {
+            return tBean.findRange(first, pageSize);
+        }
+        return Collections.EMPTY_LIST;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Response findById(@PathParam("id") final Long idTaxonomia) {
+        if (idTaxonomia != null) {
+            Taxonomia found = tBean.findById(idTaxonomia);
+            if (found != null) {
+                return Response.status(Response.Status.OK)
+                        .entity(found)
+                        .build();
+            }
+            return Response.status(Response.Status.NOT_FOUND)
+                    .header("not-found", "id")
+                    .build();
+        }
+        return Response.status(422)
+                .header("missing-parameter", "id")
+                .build();
+    }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -50,6 +86,7 @@ public class TaxonomiaResource implements Serializable {
                     .build();
         }
         try {
+            taxonomia.setIdDocumento(new Documento(idDocumento));
             tBean.create(taxonomia);
 
             URI requestUri = info.getRequestUri();
@@ -60,9 +97,11 @@ public class TaxonomiaResource implements Serializable {
                     .build();
         } catch (Exception ex) {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+            ex.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .header(RestResourceHeaderPattern.DETALLE_PARAMETRO_EQUIVOCADO, "Error interno del servidor")
+                    .header(RestResourceHeaderPattern.DETALLE_PARAMETRO_EQUIVOCADO, "Error interno del servidor: " + ex.getMessage())
                     .build();
         }
     }
+
 }
